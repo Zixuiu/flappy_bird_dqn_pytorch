3990f119b0e02c836ec8520cf0d348d6225da671
Zixuiu <105110952+Zixuiu@users.noreply.github.com>
2023-08-13 20:40:54 +0800

    Update flappy_bird_utils.py

--- game/flappy_bird_utils.py
+++ game/flappy_bird_utils.py
@@ -1,22 +1,23 @@
 import pygame
 import sys
+
 def load():
-    # path of player with different states
+    # 玩家不同状态下的图片路径
     PLAYER_PATH = (
             'assets/sprites/redbird-upflap.png',
             'assets/sprites/redbird-midflap.png',
             'assets/sprites/redbird-downflap.png'
     )
 
-    # path of background
+    # 背景图片路径
     BACKGROUND_PATH = 'assets/sprites/background-black.png'
 
-    # path of pipe
+    # 管道图片路径
     PIPE_PATH = 'assets/sprites/pipe-green.png'
 
     IMAGES, SOUNDS, HITMASKS = {}, {}, {}
 
-    # numbers sprites for score display
+    # 数字图片用于显示分数
     IMAGES['numbers'] = (
         pygame.image.load('assets/sprites/0.png').convert_alpha(),
         pygame.image.load('assets/sprites/1.png').convert_alpha(),
@@ -30,10 +31,10 @@ def load():
         pygame.image.load('assets/sprites/9.png').convert_alpha()
     )
 
-    # base (ground) sprite
+    # 地面图片
     IMAGES['base'] = pygame.image.load('assets/sprites/base.png').convert_alpha()
 
-    # sounds
+    # 音效
     if 'win' in sys.platform:
         soundExt = '.wav'
     else:
@@ -45,30 +46,30 @@ def load():
     SOUNDS['swoosh'] = pygame.mixer.Sound('assets/audio/swoosh' + soundExt)
     SOUNDS['wing']   = pygame.mixer.Sound('assets/audio/wing' + soundExt)
 
-    # select random background sprites
+    # 随机选择背景图片
     IMAGES['background'] = pygame.image.load(BACKGROUND_PATH).convert()
 
-    # select random player sprites
+    # 随机选择玩家图片
     IMAGES['player'] = (
         pygame.image.load(PLAYER_PATH[0]).convert_alpha(),
         pygame.image.load(PLAYER_PATH[1]).convert_alpha(),
         pygame.image.load(PLAYER_PATH[2]).convert_alpha(),
     )
 
-    # select random pipe sprites
+    # 随机选择管道图片
     IMAGES['pipe'] = (
         pygame.transform.rotate(
             pygame.image.load(PIPE_PATH).convert_alpha(), 180),
         pygame.image.load(PIPE_PATH).convert_alpha(),
     )
 
-    # hismask for pipes
+    # 管道的碰撞掩码
     HITMASKS['pipe'] = (
         getHitmask(IMAGES['pipe'][0]),
         getHitmask(IMAGES['pipe'][1]),
     )
 
-    # hitmask for player
+    # 玩家的碰撞掩码
     HITMASKS['player'] = (
         getHitmask(IMAGES['player'][0]),
         getHitmask(IMAGES['player'][1]),
@@ -78,7 +79,7 @@ def load():
     return IMAGES, SOUNDS, HITMASKS
 
 def getHitmask(image):
-    """returns a hitmask using an image's alpha."""
+    """使用图像的alpha通道创建碰撞掩码"""
     mask = []
     for x in range(image.get_width()):
         mask.append([])
